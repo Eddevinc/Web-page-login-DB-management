4e1ea1c3d1af54067ebe2035ce196372f57be1b9
Aditya Sethi <sethiadi8@gmail.com>
2018-09-01 20:37:20 +0530

    bug fix

--- index.php
+++ index.php
@@ -40,7 +40,9 @@
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
     	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
+
 </div>
+
 		
 </body>
 </html>
